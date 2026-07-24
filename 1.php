78e13275568e703394e46c653be6b543686cbe35
bloxlord62922-eng <bloxlord62922@gmail.com>
2026-07-24 06:57:52 +0100

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,11 @@
+echo "# ICT-ACHIEVEMENTS-2.o" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bloxlord62922-eng/ICT-ACHIEVEMENTS-2.o.git
+git push -u origin main
+Type: CNAME
+Name/Host: bloxlord
+Value/Points to: bloxlord62922-eng.github.io
+TTL: 3600
