a6ca7556f56dc5fff17b592a3c76b94ea780f9e8
bloxlord62922-eng <bloxlord62922@gmail.com>
2026-07-24 07:05:02 +0100

    Update README.md initialization commands

--- 1.php
+++ 1.php
@@ -1,11 +1,7 @@
-echo "# ICT-ACHIEVEMENTS-2.o" >> README.md
+echo "# ICT-WEB" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/bloxlord62922-eng/ICT-ACHIEVEMENTS-2.o.git
+git remote add origin https://github.com/bloxlord62922-eng/ICT-WEB.git
 git push -u origin main
-Type: CNAME
-Name/Host: bloxlord
-Value/Points to: bloxlord62922-eng.github.io
-TTL: 3600
